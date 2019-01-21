8d37988763ec06ba5e2bf1c9dba494ec25b57e3f
DANFER <DANFER@DANFER-PC>
2019-01-21 03:26:46 -0500

    Add project files.

---
+++ ReemplazaCadena/ChangeString.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReemplazaCadena
+{
+    public class ChangeString
+    {
+        public string alfabeto= "abcdefghijklmnñopqrstuvwxyz";
+        int indiceEncontrado = -1;
+        public string build(string cadenaEvaluar)
+        {           
+            
+            string cadenaDevolver = "";
+            
+            for (int i = 0; i < cadenaEvaluar.Length; i++)
+            {
+                char caracterEvaluado = cadenaEvaluar[i];
+                if (Char.IsLetter(caracterEvaluado))
+                {
+                    if (Char.IsUpper(caracterEvaluado))
+                    {
+                        caracterEvaluado = char.ToLower(caracterEvaluado);
+                        if (caracterEvaluado == 'z')
+                        {
+                            indiceEncontrado = -1;
+                        }
+                        else
+                        {
+                            indiceEncontrado = alfabeto.IndexOf(caracterEvaluado);
+                        }
+                        
+                        cadenaDevolver = cadenaDevolver + Char.ToUpper(alfabeto[indiceEncontrado + 1]);
+                    }
+                    else
+                    {
+                        if (caracterEvaluado == 'z')
+                        {
+                            indiceEncontrado = -1;
+                        }
+                        else
+                        {
+                            indiceEncontrado = alfabeto.IndexOf(caracterEvaluado);
+                        }                       
+                        cadenaDevolver = cadenaDevolver + alfabeto[indiceEncontrado + 1];
+                    }
+                    
+                }
+                else
+                {
+                    cadenaDevolver = cadenaDevolver + caracterEvaluado;
+                }   
+            }
+
+            return cadenaDevolver;
+
+        }
+
+        static void Main(string[] args)
+        {
+            
+            ChangeString objetoPrueba = new ChangeString();
+            //string cadenaPrueba = "**Casa 52Z";
+            string cadenaPrueba = "";
+            Console.WriteLine("*****Imprimiendo Cadenas****");
+            Console.WriteLine("Ingrese una Cadena:");
+            cadenaPrueba = Console.ReadLine();
+            Console.WriteLine(objetoPrueba.build(cadenaPrueba));           
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UnitTestReemplazaCadena/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestReemplazaCadena;
+using ReemplazaCadena;
+
+namespace UnitTestReemplazaCadena
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //First  
+            string cadenaPrueba = "**Casa 52Z";
+            ChangeString objetoPrueba = new ChangeString();
+            Console.WriteLine(objetoPrueba.build(cadenaPrueba));
+        }
+
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //First  
+            string cadenaPrueba = "123 abcd*3";
+            ChangeString objetoPrueba = new ChangeString();
+            Console.WriteLine(objetoPrueba.build(cadenaPrueba));
+        }
+
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //First  
+            string cadenaPrueba = "Daniel 2019 Gutierrez";
+            ChangeString objetoPrueba = new ChangeString();
+            Console.WriteLine(objetoPrueba.build(cadenaPrueba));
+        }
+    }
+}
